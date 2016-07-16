fa9116546cf3d08fbf6fb3f7873c45ccb6a5dc15
Abd Elrahman Mohamed <abdelrahmanzied@gmail.com>
2016-07-16 15:53:39 +0200

    Main Files
    Added index.html, css,, js, fonts and images

--- js/script.js
+++ js/script.js
@@ -9,4 +9,10 @@ $(function () {
     $('.site-nav-list li').click(function(){
         $(this).addClass('active-link').siblings().removeClass('active-link');
     });
+});
+
+// nav icon
+
+$('.handle').on('click', function(){
+	$('site-nav-list').toggleClass('showing');
 });    
